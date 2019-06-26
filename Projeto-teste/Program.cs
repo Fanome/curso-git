72c5117345046b70acbd9bc192f5523f4aa130d1
fanome <fabionogueiram@yahoo.com.br>
2019-06-26 14:41:18 -0300

    Acrecentado msg Bom dia

--- Projeto-teste/Program.cs
+++ Projeto-teste/Program.cs
@@ -11,6 +11,7 @@ namespace Projeto_teste
         static void Main(string[] args)
         {
             Console.WriteLine("Ola mundo");
+            Console.WriteLine("Bom dia");
 
             Console.ReadLine();
         }
